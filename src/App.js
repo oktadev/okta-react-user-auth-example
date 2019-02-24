ea437e811d9260a59d16c01fa97ce32a95e39db6
Braden M. Kelley <redbmk@gmail.com>
2019-02-24 12:53:51 -0800

    fetch chuck norris quotes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { decode } from 'he';
+
+import logo from './chuck-norris.png';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [joke, setJoke] = useState('');
+
+  const fetchJoke = async () => {
+    const response = await fetch('https://api.icndb.com/jokes/random');
+    const { value } = await response.json();
+
+    setJoke(decode(value.joke));
+  };
+
+  useEffect(() => {
+    if (!joke) {
+      fetchJoke();
+    }
+  }, [joke]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{joke || '...'}</p>
+        <button className="App-link" onClick={() => setJoke('')}>
+          Get a new joke
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
